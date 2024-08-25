821620cb2dd9f0e25a9c02c9609dd2ebde47f96a
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-08-25 17:24:17 +0530

    up

--- MovieMania/src/components/leftBar.jsx
+++ MovieMania/src/components/leftBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import "../styles/leftBar.css";
 
 const LeftBar = () => {
-  return <div></div>;
+  return <div className="box">I am left bar niga</div>;
 };
 export default LeftBar;
--- MovieMania/src/home.jsx
+++ MovieMania/src/home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import NavBar from "./components/nav_bar";
+import LeftBar from "./components/leftBar";
+// import
+import "./styles/home.css";
 
 const Home = () => {
   return (
-    <div>
-      <NavBar></NavBar>
+    <div className="mainlayout">
+      <div className="drawer">
+        <LeftBar />
+      </div>
+      <div className="content">
+        <NavBar />
+        <div className="moviesArea" />
+      </div>
     </div>
   );
 };
