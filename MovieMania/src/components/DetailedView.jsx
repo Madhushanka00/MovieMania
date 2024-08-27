3f9f0a0663d31d03a977c374d3d3fa09346b1d05
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-08-27 23:25:43 +0530

    updates done today. detailed window done

--- MovieMania/src/components/DetailedView.jsx
+++ MovieMania/src/components/DetailedView.jsx
@@ -1,6 +1,8 @@
 import react from "react";
 import { useState, useEffect } from "react";
 import "../styles/DetailedView.css";
+import StarIcon from "@mui/icons-material/Star";
+import CloseIcon from "@mui/icons-material/Close";
 
 const DetailedView = () => {
   const [movie, setMovie] = useState("");
@@ -19,25 +21,57 @@ const DetailedView = () => {
   }, [movieId]);
 
   return (
-    <div className="glass">
-      {movie ? (
-        <div className="Construction">
-          <img
-            // src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={`${movie.title} poster`}
-            className="movie-backdrop"
-          />
-          <div className="DetailsDection">
-            <div className="titleSection">{movie.title}</div>
+    <>
+      <div className="overlay"></div>
+      <div className="glass">
+        {movie ? (
+          <div className="Construction">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={`${movie.title} poster`}
+              className="movie-backdrop"
+            />
+            <div className="DetailsSection_">
+              <CloseIcon className="close" />
+              <div className="titleSection_">
+                <h3>{movie.title}</h3>
+                <div className="releaseDate">
+                  <h6>{movie.tagline}</h6>
+                  <h6 className="date">{movie.release_date}</h6>
+                </div>
+              </div>
+              <div className="wrapAround">
+                <div className="ratingSection">
+                  <StarIcon className="star" />
+                  {"  "}
+                  {movie.vote_average}
+                </div>
+                <div className="genras">
+                  {movie.genres && movie.genres.length > 0 ? (
+                    movie.genres.map((genre, index) => (
+                      <span key={genre.id} className="genre">
+                        {genre.name}
+                        {index < movie.genres.length - 1 && " | "}
+                      </span>
+                    ))
+                  ) : (
+                    <span>No genres available</span>
+                  )}
+                </div>
+              </div>
+              <div className="Overview">
+                <h3 className="ttle">Overview</h3>
+                <p>{movie.overview}</p>
+              </div>
 
-            <p>{movie.overview}</p>
-            {/* Render other movie details here */}
+              {/* Render other movie details here */}
+            </div>
           </div>
-        </div>
-      ) : (
-        <p className="loadingScreen">Loading...</p>
-      )}
-    </div>
+        ) : (
+          <p className="loadingScreen">Loading...</p>
+        )}
+      </div>
+    </>
   );
 };
 export default DetailedView;
