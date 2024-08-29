2d784c1e1a2e0265dbe59c651bf2fb1017685111
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-08-29 16:12:02 +0530

    install

--- MovieMania/src/components/Asistant.jsx
+++ MovieMania/src/components/Asistant.jsx
@@ -12,7 +12,11 @@ const Asistant = () => {
               Hi, I am FilmSeeker, your personal movie assistant. How can I help
             </div>
             <div className="user">
-              Hi, I am looking for a movie to watch with my family
+              Hi, I am looking for a movie to watch with my family Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Officiis fuga rerum
+              sit neque autem modi minima facere quaerat dignissimos molestiae
+              aspernatur tempore sed tenetur dolores, esse voluptatibus
+              reiciendis corporis natus!
             </div>
           </div>
           <div className="chatInput">
