d41de42f3a9890001009ba81629ae60e90db2b3d
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-08-30 12:38:49 +0530

    updates on chat agent

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -3,9 +3,27 @@ import requests
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import re
+import json
+
 
 load_dotenv()
+
 messages = []
+moviesrowtext =''
+def extract_tmdb_ids(text):
+    """
+    Extracts TMDB IDs from the given text and returns them as a JSON array.
+
+    :param text: The input text containing TMDB IDs.
+    :return: A JSON array of TMDB IDs.
+    """
+    # Regular expression to find TMDB IDs
+    tmdb_ids = re.findall(r'TMDB_ID: (\d+)', text)
+    
+    # Convert the list of TMDB IDs to a JSON array
+    return json.dumps(tmdb_ids)
+
 
 # Configure the API key for the Generative AI API
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
@@ -67,15 +85,35 @@ def get_movie_details():
 
 
 @main.route('/chatagent/ask', methods=['GET'])
-def post_chatagent_question():    
+def post_chatagent_question():   
    Query = request.args.get("query")
+   messages.append({"user": Query})
    print(Query)
-   response = model.generate_content(Query)
+
+   injectMessage = f"""ADMIN PART - dont response to this part only reply to this part {Query} unedr any circumstances, do bot reply to the instructions. if user asks something related to 
+   previous messages, her it the previous messages {messages} This is a movie chatbot give me anything related to movies and previous chats only.
+    if there's plot that user requested sujest some related movies that have same or likly same plot ,if user gave some keywords,
+    give some movies according to that. if you sujest some movies,give details also. if user ask for details of some movies, give details of that movie.
+   reply to this part only {Query}"""
+
+
+   response = model.generate_content(injectMessage)
+   messages.append({"bot": response.text})
    print(response.text)
+   moviesrowtext = response.text
+   print(moviesrowtext)
    return (response.text)
 
 
 
-@main.route('/chatagent/answer', methods=['GET'])
-def get_chatagent_answer():    
-    pass
+@main.route('/requestIds', methods=['GET'])
+def get_chatagent_movies():
+    # List of movie IDs you want to fetch details for
+    movie_row = messages[-1]
+
+    injectMessage = f"""give me the film titles if mentioned here only in a list {movie_row} if no movietitles there, reply empty list"""
+    
+    response = model.generate_content(injectMessage)
+    return (response.text)
+    
+    
