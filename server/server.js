8a8996f9239d9c144687f58d5b631ca2feed8168
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-09-05 15:31:20 +0530

    bcrypt

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const CORS = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
+// const app = express();
 
 const uri = "mongodb+srv://Mahesha:Tg%23078DB@cluster0.wgivi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const app = express();
+app.use(CORS);
+app.use(bodyParser.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
