ba6f4c593426c77514c787e40b0619e7bb0fca60
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-09-07 16:23:51 +0530

    slide in slide out fixed

--- MovieMania/src/components/profile.jsx
+++ MovieMania/src/components/profile.jsx
@@ -5,12 +5,15 @@ import "../styles/profile.css";
 
 const Profile = ({ details, setProfile, profile }) => {
   console.log("Details:", details);
+  const [isVisible, setIsVisible] = useState(profile);
 
   const profileRef = useRef(null);
 
   const handleClickOutside = (event) => {
     if (profileRef.current && !profileRef.current.contains(event.target)) {
-      setProfile(false); // Close the profile when clicked outside
+      setIsVisible(false); // Close the profile when clicked
+      setTimeout(() => setProfile(false), 500);
+      // setProfile(false); // Close the profile when clicked outside
     }
   };
 
@@ -21,11 +24,23 @@ const Profile = ({ details, setProfile, profile }) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+  //   // Monitor changes in `profile` prop to trigger slide out and delay removal
+  //   useEffect(() => {
+  //     if (profile) {
+  //       setIsVisible(true); // Show profile (slide-in)
+  //     } else {
+  //       setTimeout(() => setIsVisible(false), 500); // Delay hiding profile to allow slide-out animation to finish
+  //     }
+  //   }, [profile]);
+
+  //   if (!isVisible) {
+  //     return null; // Remove the component from the DOM when the animation completes
+  //   }
 
   return (
     <div
       ref={profileRef}
-      className={`ProfileArea ${profile ? "slide-in" : "slide-out"}`}
+      className={`ProfileArea ${isVisible ? "slide-in" : "slide-out"}`}
     >
       <h1>Profile</h1>
       <h2>UserId: {details.userId}</h2>
