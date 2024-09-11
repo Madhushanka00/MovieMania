ef02bcf1543ce95d9540cdfbc57e7aeea394b347
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-09-11 15:37:06 +0530

    up

--- Recommender/recSys.py
+++ Recommender/recSys.py
@@ -7,17 +7,32 @@ uri = "mongodb+srv://Mahesha:Tg%23078DB@cluster0.wgivi.mongodb.net/?retryWrites=
 # Initialize the Flask app
 app = Flask(__name__)
 
+
+
 # Create a MongoClient instance
 client = MongoClient(uri)
+
+# Create a MongoClient instance
+try:
+    client = MongoClient(uri)
+    # Verify connection
+    client.admin.command('ping')
+    print("MongoDB connected successfully!")
+except ConnectionError as e:
+    print(f"Could not connect to MongoDB: {e}")
 
 # Define the MongoDB database
 mdb = client['MovieMania']
 
 
-# Define a route for the home page
 @app.route('/')
-def home():
-    return 'Hello, Flask!'
+def check_connection():
+    try:
+        # Example operation: list collections in the 'MovieMania' database
+        collections = mdb.list_collection_names()
+        return f"Connected to MongoDB! Collections in MovieMania DB: {collections}"
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 # Run the app if the script is executed directly
 if __name__ == '__main__':
