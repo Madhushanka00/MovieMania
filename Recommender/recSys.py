afc10bec2d958aef64f4a2c8aaf3ac1b94fcee47
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-09-11 14:19:31 +0530

    pip installs

--- Recommender/recSys.py
+++ Recommender/recSys.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+# MongoDB connection string (replace with your own URI)
+uri = "mongodb+srv://Mahesha:Tg%23078DB@cluster0.wgivi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Initialize the Flask app
 app = Flask(__name__)
+
+# Create a MongoClient instance
+client = MongoClient(uri)
+
+# Define the MongoDB database
+mdb = client['MovieMania']
+
 
 # Define a route for the home page
 @app.route('/')
