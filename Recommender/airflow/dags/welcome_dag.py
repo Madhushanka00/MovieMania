c0b7dafde5310d42ff91ff24da38cd98fefe3c3c
Mahesha Madhushanka <e19226@eng.pdn.ac.lk>
2024-09-19 22:55:00 +0530

    ranking model save

--- Recommender/airflow/dags/welcome_dag.py
+++ Recommender/airflow/dags/welcome_dag.py
@@ -5,15 +5,22 @@ from datetime import datetime
 import requests
 import papermill as pm
 
-def run_notebook():
+def run_notebook_retrival():
     pm.execute_notebook(
         '/opt/airflow/dags/RetrivalModel_tfrs.ipynb',  # Path inside the container
         '/opt/airflow/dags/RetrivalOutput.ipynb'     # Output file inside the container
 
     )
 
+def run_notebook_ranking():
+    pm.execute_notebook(
+        '/opt/airflow/dags/RankingModel_tfrs.ipynb',  # Path inside the container
+        '/opt/airflow/dags/RankingOutput.ipynb'     # Output file inside the container
+
+    )
+
 def print_welcome():
-    print('Welcome to Airflow!')
+    print('Pipeline started')
 
 def print_date():
     print('Today is {}'.format(datetime.today().date()))
@@ -50,12 +57,17 @@ print_welcome_task = PythonOperator(
 
 # )
 # Define the task
-run_notebook_task = PythonOperator(
-    task_id='run_jupyter_notebook',
-    python_callable=run_notebook,
+run_notebook_retrival_task = PythonOperator(
+    task_id='Train_retival_model',
+    python_callable=run_notebook_retrival,
     dag=dag
 )
 
+run_notebook_ranking_task = PythonOperator(
+    task_id='Train_ranking_model',
+    python_callable=run_notebook_ranking,
+    dag=dag
+)
 
 
 # print_random_quote = PythonOperator(
@@ -72,4 +84,4 @@ run_notebook_task = PythonOperator(
 
 # Set the dependencies between the tasks
 
-print_welcome_task >> run_notebook_task
+print_welcome_task >> run_notebook_retrival_task >> run_notebook_ranking_task
